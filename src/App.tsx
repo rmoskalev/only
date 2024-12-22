18424815cb08ac644b1cc1fce920f475448548bf
Ruslan <rmoskalevdev@gmail.com>
2024-12-22 06:46:35 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/styles.scss";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,66 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { Configuration } from "webpack";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import "webpack-dev-server";
+
+const config: Configuration = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: path.join(__dirname, "public"),
+    compress: true,
+    port: 3000,
+    hot: true,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
+
+export default config;
